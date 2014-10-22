37d5a5f07124c5a666cdaf2a8c5052ffbe9d1a21
lukacvetkovic <l.cvetk@gmail.com>
2014-10-22 18:24:26 +0200

    App radi ful screen

--- app/src/main/java/cvim/hr/samobus/MainActivity.java
+++ app/src/main/java/cvim/hr/samobus/MainActivity.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -39,6 +42,14 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+         /*
+        Da bude sve full screen -- ova 2 reda ispod
+         */
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_main);
 
         departureHelper = new NewDepartureHelper(this);
